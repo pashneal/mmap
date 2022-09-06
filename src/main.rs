8c04a1c96aef595f6112d499812186bcb22aa354
Ashneal Powell <pashneal@gmail.com>
2022-09-06 01:48:33 -0400

    Add target

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+use std::ptr::read_volatile;
+
+fn main(){
+    let pru_start = (0x4A30_0000 + 0x200/4) as *mut u32;
+    unsafe{ println!("{:#08x}", read_volatile(pru_start));}
 }
