bd78520068eca0c63dd0411f28334bcebfd93f5d
Ashneal Powell <pashneal@gmail.com>
2022-09-06 01:52:44 -0400

    New release

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::ptr::read_volatile;
 
 fn main(){
-    let pru_start = (0x4A30_0000 + 0x200/4) as *mut u32;
+    let pru_start = (0x4A30_0000 + 0x200) as *mut u32;
     unsafe{ println!("{:#08x}", read_volatile(pru_start));}
 }
